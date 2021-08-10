d36a74ab24546e507e6ee8ce0c28114b3e5a6e61
Felipe Ferreira <frisney@pm.me>
2021-08-09 23:23:32 -0300

    Desenvolvimento da interface e Metodo de Calculo do IMC

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,20 @@
 package application;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MainController {
+	public TextField txtAltura;
+	public TextField txtPeso;
+	public Button btnCalc;
+	public Label lblResult;
 	
+	@SuppressWarnings("unused")
+	public void calcular() {
+		Double imc = Double.parseDouble(txtPeso.getText());
+		imc /= Math.pow(Double.parseDouble(txtAltura.getText()), 2f);
+		lblResult.setText(String.format("%.2f",imc));
+		System.out.println(String.format("%.2f",imc));
+	}
 }
