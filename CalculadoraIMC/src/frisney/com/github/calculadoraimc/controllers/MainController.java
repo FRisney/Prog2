2ce41dc2726519ad80654e2cdcc64d12679e4530
Felipe <frisney@pm.me>
2021-08-16 21:51:54 -0300

    Melhorias

--- CalculadoraIMC/src/frisney/com/github/calculadoraimc/controllers/MainController.java
+++ CalculadoraIMC/src/frisney/com/github/calculadoraimc/controllers/MainController.java
@@ -2,6 +2,8 @@ package frisney.com.github.calculadoraimc.controllers;
 
 import frisney.com.github.calculadoraimc.models.Genero;
 import frisney.com.github.calculadoraimc.models.Masculino;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -10,6 +12,7 @@ public class MainController {
 	public TextField txtAltura;
 	public TextField txtPeso;
 	public Button btnCalc;
+	public Button btnClear;
 	public Label lblResult;
 	
 	@SuppressWarnings("unused")
@@ -20,7 +23,23 @@ public class MainController {
 
 		Genero genero = new Masculino();
 
-		lblResult.setText(genero.testCategoria(imc));
+		lblResult.setText(String.format("%,.2f", imc));
+		System.out.println(String.format("%,.2f", imc));
 		System.out.println(genero.testCategoria(imc));
+
+		Alert alerta = new Alert(AlertType.CONFIRMATION);
+		alerta.setTitle("Resultado");
+		alerta.setContentText(genero.testCategoria(imc));
+		alerta.show();
+	}
+	
+	public void limpar() {
+		lblResult.setText("...");
+		txtPeso.setText("");
+		txtAltura.setText("");
+		Alert alerta = new Alert(AlertType.CONFIRMATION);
+		alerta.setTitle("Limpei os campos");
+		alerta.setContentText("Limpei os campos");
+		alerta.show();
 	}
 }
